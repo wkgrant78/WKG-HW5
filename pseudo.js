0a68039689bd401b0e28557807c3e8600b091f1a
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-01 22:48:42 -0500

    Added pseudo code

---
+++ pseudo.js
@@ -0,0 +1,39 @@
+// HTML Elements:
+
+// Jumbotron (given)
+
+// Current day and time
+
+// Container: 
+
+    // 9 rows: 8:00 AM - 5:00 PM
+    // 3 col: time/text/btn
+
+// --------------------------------------
+
+// Script:
+
+// define var workDay = hour []  --------------> "task"
+
+// Auto update date & time (moment.js library) 
+
+// add objects (i.e. workDay) to local storage - use 'JSON.stringify' to convert "task" object to JSON string required for local storage
+    // localStorage.setItem("workDay", JSON.stringify(newStudent));
+
+
+// Retrieve the object from storage to add a new workDay
+    // var retrievedObject = localStorage.getItem("workDay");
+    // var stored          = JSON.parse(retrievedObject);
+
+// add new workDay
+    // var workDay
+
+
+
+// on click event - use $(".saveBtn").click (probably going to use $(this))
+
+// past/present/future time attr to hr rows in container -> (60 min * 60 s * 1000 ms) = 3600000 ms 
+    // setTimeout(function())
+        // setInterval( , 3600000) 
+
+
