13c1c4bbb8eb6434b801ea670c5400c83b0b0b77
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-01 22:50:54 -0500

    Deleted duplicate script

--- script.js
+++ script.js
@@ -87,38 +87,3 @@ for(const property in workDay) {
     })
   }
   
-
-
-
-      // // conditional (ternary) operator is the only JavaScript operator that takes three operands: a condition followed by a question mark ( ? ), then an expression to execute if the condition is truthy followed by a colon ( : ), and finally the expression to execute if the condition is falsy
-      // function loadCorrectDataset() {
-      //   result = localStorage.getItem('workDay')
-      //   return (result ? result : workDay);
-      // }
-      
-      // function initializeLocalStorage() {
-      //   localStorage.setItem('workDay', JSON.stringify(workDay));
-      // };
-      
-      // function saveToLocalStorage(dayObj) {
-      //   localStorage.setItem('workDay', JSON.stringify(dayObj));
-      // }
-      // //  
-      // function saveSchedule(workDay, val) {
-      //   if(!localStorage.getItem('workDay')) {
-      //     initializeLocalStorage();
-      //   }
-      
-      //   let workHours = JSON.parse(localStorage.getItem('workDay'));
-      //   workHours[workDay] = val
-      
-      //   saveToLocalStorage(workHours);
-      // }
-      
-      // function updateCalendartoDo(dayObject) {
-      //   $(".calendar-row").each(function(index) {
-      //     let res = $(this).children("div");
-      //     $(this).children("textarea").text(dayObject[res.text()]);
-      //   })
-      // }
-      
